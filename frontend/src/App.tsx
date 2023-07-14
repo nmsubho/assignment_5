d5fe52c9e2ba8a64cad2f00ea6cae8596b9bcdd4
Md. Nagib Mahfuz Subho <nmsubho@gmail.com>
2023-07-14 22:26:15 +0600

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+
+function App() {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <div className="flex justify-center items-center mt-5">
+        <button
+          className="border-2 p-2 border-red-500 text-red-500"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <div className="px-3 text-blue-700">{count}</div>
+        <button
+          className="border-2 p-2 border-green-400 text-green-400"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <button
+          className="border-2 p-2 border-green-400 text-green-400"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          increment By 5
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
