923d5ce71cbf12013db9ba4314e0e5f10ab43b08
Md. Nagib Mahfuz Subho <nmsubho@gmail.com>
2023-07-15 22:16:42 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
 import MainLayout from "./layout/MainLayout";
+import { useAppDispatch } from "./redux/hooks";
+import { setLoading, setUser } from "./redux/features/user/userSlice";
+import { auth } from "./lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setLoading(true));
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser({ email: user.email, uid: user.uid }));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, [dispatch]);
   return (
     <div>
       <MainLayout />
